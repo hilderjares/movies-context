2eaa989de2d49b468bbfe34f43a843a9a49a0177
hilderjares <hilderjares>
2020-02-16 12:24:57 -0300

    movies mock

---
+++ src/mock.js
@@ -0,0 +1,27 @@
+export const MoviesMock = [
+    {
+        id: 1,
+        name: "Avengers",
+        price: 20.2,
+    },
+    {
+        id: 2,
+        name: "Django Unchained",
+        price: 30,
+    },
+    {
+        id: 3,
+        name: "Saving Private Ryan",
+        price: 50,
+    },
+    {
+        id: 4,
+        name: "Batman",
+        price: 12.3,
+    },
+    {
+        id: 5,
+        name: "Toy Story",
+        price: 31,
+    },
+];
